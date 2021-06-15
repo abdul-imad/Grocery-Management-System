72d7685b8145d9f1f72bb6b413bc75bc6cf9a159
Abdul Imad <abdimad786@gmail.com>
2021-06-15 20:03:26 +0530

    grocery management project

---
+++ itp/admin/footer.inc.php
@@ -0,0 +1,33 @@
+<div class="clearfix"></div>
+<footer class="site-footer">
+    <div class="footer-inner bg-white">
+        <div class="row">
+            <div class="col-sm-6">
+                <center>
+                    <h5><b>Made with <span style="font-size: 20px ;color: #00BFFF;">&#10084;</span> by our team &copy;<?php echo date('Y') ?></b></h5>
+                </center>
+                <p>
+                    <center>
+                        <h5><b>Built with pure HTML, CSS, PHP, JavaScript and Bootstrap</b></h5>
+                    </center>
+                </p>
+                <p></p>
+                <h5><b><a href="mailto:ourteam@gmail.com">majorproject2021@gmail.com</a></b></h5>
+                <p></p>
+                <h5><b><a href="" target="_blank"> INSTAGRAM</a></b></h5>
+                <p></p>
+                <h5><b><a href="" target="_blank">FACEBOOK</a></b></h5>
+                <p></p>
+                <h5><b><a href="" target="_blank"> LINKEDIN</a></b></h5>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+<script src="assets/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
+<script src="assets/js/popper.min.js" type="text/javascript"></script>
+<script src="assets/js/plugins.js" type="text/javascript"></script>
+<script src="assets/js/main.js" type="text/javascript"></script>
+</body>
+
+</html>
